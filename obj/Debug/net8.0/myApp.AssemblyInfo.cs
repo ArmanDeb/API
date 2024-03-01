9564a010cd06777827ba0634efd725872e62278c
ArmanDeb <120381149+ArmanDeb@users.noreply.github.com>
2024-03-01 12:42:57 +0100

    css
    black nav bar

--- obj/Debug/net8.0/myApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/myApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9984e34d102f0252013a1d59214b3fb3bd0d1d6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+013511bb62d30179047a5da25243deb30681f699")]
 [assembly: System.Reflection.AssemblyProductAttribute("myApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
